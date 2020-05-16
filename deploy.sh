49ca51d2660ce37134f8a38130712c7e6d4695e9
Ben Zhang <benzhang42@gmail.com>
2020-05-16 12:05:25 -0400

    change email in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 git config user.name "Circle CI"
-git config user.email "kpeng97@gmail.com"
+git config user.email "b334zhan@gmail.com"
 
 message="Deploy commit $(git rev-parse HEAD)"
 echo "Deploying $(git rev-parse HEAD) to master"
@@ -10,5 +10,5 @@ rm .gitignore # We want all compiled assets! ALL OF THEM
 mv .gitignore.deploy .gitignore
 git add --all
 git commit -m "$message"
-git push --force "git@github.com:kevintpeng/Learn-Something-Every-Day.git" develop:master
+git push --force "git@github.com:ben-zhang/Learn-Something-Every-Day.git" develop:master
 echo "Deployed! Going to sleep..."
